b2ae92de5e4384bf05b1b933aea1bf0a5d2d6b30
moin1223 <moinpuccse@gmail.com>
2021-10-26 23:49:53 +0600

    navbar done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./app.scss"
+import Home from './pages/Home/Home';
+
+const App = () => {
+  return (
+    <div>
+      <Home></Home>
+    </div>
+  );
+};
+
+export default App;
