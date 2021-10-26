ea54cb30389df5fa1807356ad0644bc159fd3143
moin1223 <moinpuccse@gmail.com>
2021-10-27 00:24:56 +0600

    navbar update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,16 @@
 import { ArrowDropDown, Notifications, Search } from '@material-ui/icons';
+import { useState } from 'react';
 import './navbar.scss'
 
 const Navbar = () => {
+  const [isScrolled,setIsScrolled] = useState(false);
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
+  console.log(isScrolled)
   return (
-    <div className="navbar">
+    <div className={isScrolled ? "navbar scrolled" : "navbar"}>
       <div className="container">
         <div className="left">
         <h1 className="logo"><i>VISIONFLIX</i></h1>
