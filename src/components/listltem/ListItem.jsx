c628a3a87770abe2c2590f89ed178f429963cf15
moin1223 <moinpuccse@gmail.com>
2021-10-27 23:14:57 +0600

    slider done

---
+++ src/components/listltem/ListItem.jsx
@@ -0,0 +1,12 @@
+import "./listItem.scss";
+
+const ListItem = () => {
+    return (
+        <div className="listItem">
+            item
+            
+        </div>
+    );
+};
+
+export default ListItem;
